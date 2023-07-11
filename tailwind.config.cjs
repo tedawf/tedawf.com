9f38c02ef9270ed1877e15efd275a5422a99f132
Ang Wei Feng (Ted) <ted.awf@outlook.com>
2023-07-11 23:11:23 +0800

    look more like cards

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        bkg: "rgb(var(--color-bkg) / <alpha-value>)",
+        content: "rgb(var(--color-content) / <alpha-value>)",
+      },
+    },
+  },
+  plugins: [],
+};
