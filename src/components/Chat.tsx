522247f75f7d772010f74bf1944a51d2ca14ec79
realguse <me@realguse.com>
2025-08-17 20:43:33 +0200

    fix: rename unexistent class

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -4,52 +4,52 @@ import ChatHeader from "./ChatHeader";
 import ChatInput from "./ChatInput";
 import ChatMessages from "./ChatMessages";
 import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
 } from "./ui/Accordion";
 
 export default function Chat() {
-  const {
-    messages,
-    input,
-    handleInputChange,
-    handleSubmit,
-    setMessages,
-    isLoading,
-    error,
-  } = useChat();
+    const {
+        messages,
+        input,
+        handleInputChange,
+        handleSubmit,
+        setMessages,
+        isLoading,
+        error,
+    } = useChat();
 
-  const { isVisible } = useChatbot();
+    const { isVisible } = useChatbot();
 
-  return (
-    isVisible && (
-      <Accordion type="single" collapsible className="flexs relative z-40">
-        <AccordionItem
-          value="item-1"
-          className="fixed bottom-8 right-8 w-80 rounded-md border bg-background"
-        >
-          <AccordionTrigger className="border-b px-6">
-            <ChatHeader />
-          </AccordionTrigger>
-          <AccordionContent className="flex max-h-96 min-h-80 flex-col justify-between p-0">
-            <ChatMessages
-              messages={messages}
-              error={error}
-              isLoading={isLoading}
-            />
-            <ChatInput
-              input={input}
-              handleSubmit={handleSubmit}
-              handleInputChange={handleInputChange}
-              setMessages={setMessages}
-              isLoading={isLoading}
-              messages={messages}
-            />
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-    )
-  );
+    return (
+        isVisible && (
+            <Accordion type="single" collapsible className="flex relative z-40">
+                <AccordionItem
+                    value="item-1"
+                    className="fixed bottom-8 right-8 w-80 rounded-md border bg-background"
+                >
+                    <AccordionTrigger className="border-b px-6">
+                        <ChatHeader />
+                    </AccordionTrigger>
+                    <AccordionContent className="flex max-h-96 min-h-80 flex-col justify-between p-0">
+                        <ChatMessages
+                            messages={messages}
+                            error={error}
+                            isLoading={isLoading}
+                        />
+                        <ChatInput
+                            input={input}
+                            handleSubmit={handleSubmit}
+                            handleInputChange={handleInputChange}
+                            setMessages={setMessages}
+                            isLoading={isLoading}
+                            messages={messages}
+                        />
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
+        )
+    );
 }
