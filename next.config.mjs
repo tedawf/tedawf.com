8e527bfac5b9be497426d1b832acea71a890420a
tedawf <hello@tedawf.com>
2025-09-14 21:13:23 +0800

    add image domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "blog.tedawf.com",
+        port: "",
+        pathname: "/images/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
