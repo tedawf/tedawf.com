e4a01a8eefea88665d662bcfb7e683c7f9d4c26c
tedawf <hello@tedawf.com>
2025-11-17 22:36:42 +0800

    feat: add support for GFM tables in MDX content

--- src/components/MDXContent.tsx
+++ src/components/MDXContent.tsx
@@ -1,25 +1,89 @@
 import { MDXRemote, MDXRemoteProps } from "next-mdx-remote/rsc";
 import { JSX } from "react";
+import remarkGfm from "remark-gfm";
 import { highlight } from "sugar-high";
 import Counter from "./Counter";
+import {
+  Table as UITable,
+  TableBody as UITableBody,
+  TableCell as UITableCell,
+  TableHead as UITableHead,
+  TableHeader as UITableHeader,
+  TableRow as UITableRow,
+} from "./ui/table";
+import { cn } from "@/lib/utils";
 
 function Code({ children, ...props }: any) {
   let codeHTML = highlight(children);
   return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />;
 }
 
+function Table(props: JSX.IntrinsicElements["table"]) {
+  const { className, ...rest } = props;
+  return (
+    <div className="my-8">
+      <UITable
+        className={cn(
+          "w-full text-sm [&_td]:px-4 [&_td]:py-3 [&_th]:px-4 [&_th]:py-3",
+          className,
+        )}
+        {...rest}
+      />
+    </div>
+  );
+}
+
+function TableHeaderSection(props: JSX.IntrinsicElements["thead"]) {
+  const { className, ...rest } = props;
+  return (
+    <UITableHeader
+      {...rest}
+      className={cn(
+        "bg-muted/70 text-foreground [&_tr]:border-b [&_tr]:border-border/60",
+        "[&_th]:font-semibold [&_th]:uppercase [&_th]:tracking-wide",
+        className,
+      )}
+    />
+  );
+}
+
 const components = {
   code: Code,
   Counter,
+  table: Table,
+  thead: TableHeaderSection,
+  tbody: UITableBody,
+  th: UITableHead,
+  td: UITableCell,
+  tr: UITableRow,
 };
 
 export default function MDXContent(
   props: JSX.IntrinsicAttributes & MDXRemoteProps,
 ) {
+  const { components: componentsProp, options: optionsProp, ...rest } = props;
+
+  const mergedComponents = {
+    ...components,
+    ...(componentsProp || {}),
+  };
+
+  const mergedOptions: MDXRemoteProps["options"] = {
+    ...(optionsProp || {}),
+    mdxOptions: {
+      ...(optionsProp?.mdxOptions || {}),
+      remarkPlugins: [
+        remarkGfm,
+        ...(optionsProp?.mdxOptions?.remarkPlugins || []),
+      ],
+    },
+  };
+
   return (
     <MDXRemote
-      {...props}
-      components={{ ...components, ...(props.components || {}) }}
+      {...rest}
+      components={mergedComponents}
+      options={mergedOptions}
     />
   );
 }
