6234653d80154e056e065b14a44e6d072da40cff
tedawf <hello@tedawf.com>
2026-01-04 02:13:07 +0800

    optimise: compress image sizes

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -29,7 +29,7 @@ export function ProjectCard({ project }: Props) {
               alt={name}
               width={500}
               height={300}
-              sizes="(max-width: 640px) 100vw, 384px"
+              sizes="(max-width: 640px) calc(100vw - 4rem), 344px"
               quality={80}
               className="h-40 w-full object-cover object-top"
             />
